7a38c2f605d966d41d67b63c2bbfce711543f3b3
Somin <sominupadhyaya@gmail.com>
2022-06-22 13:53:33 +0545

    start bug-fix

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//not really efficient
+// function addNums(a,b){ //f(a,b)
+//     return a+b
+// }
+// const c = addNums(12,13)
+// console.log(c) //25
 
-function addNums(a,b){ //f(a,b)
-    return a+b
-}
-const c = addNums(12,13)
-console.log(c) //25
+const addNums =(a,b) => a+b
+console.log(addNums(12,13))

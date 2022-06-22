0153e6244548f334e2d01a05d2ee91524e0a83cf
Somin <sominupadhyaya@gmail.com>
2022-06-22 15:29:35 +0545

    complete map function

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-//not really efficient
-// function addNums(a,b){ //f(a,b)
-//     return a+b
-// }
-// const c = addNums(12,13)
-// console.log(c) //25
 
-const addNums =(a,b) => a+b
-console.log(addNums(12,13))
+
+const arr = [1,2,3,4,5]
+
+const iterator = num => num+5
+const newArr = arr.map(iterator)
+//1st itr => newArr[] => newArr[6]
+console.log(newArr);
+
+const array = [
+{name : 'Somin',grade : 12,isDead : false},
+{name : 'Surab',grade : 13,isDead : false},
+{name : 'Oscar',grade : 14,isDead : false},
+{name : 'Salman',grade : 15,isDead : false},
+]
+
+
+const  gradeArray = array.map(({grade}) => grade)
+
+console.log(gradeArray)
+
+
+
